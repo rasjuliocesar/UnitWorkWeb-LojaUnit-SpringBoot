115e70056907e1c6a67bddc3f4061c07cff771f4
Julio Cesar <rasjuliocesar@gmail.com>
2020-10-31 11:27:35 -0300

    Projeto Iniciado

---
+++ src/main/java/com/lojaunit/UnitWorkWebLojaUnitSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.lojaunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitWorkWebLojaUnitSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitWorkWebLojaUnitSpringBootApplication.class, args);
+	}
+
+}
