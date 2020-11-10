044a0bff2e58065d8de3b8f813cf0b6e27398de7
Julio Cesar <rasjuliocesar@gmail.com>
2020-11-10 20:46:52 -0300

    Post/Delete e Put - Clientes

--- src/main/java/com/lojaunit/controller/ClienteController.java
+++ src/main/java/com/lojaunit/controller/ClienteController.java
@@ -1,13 +1,19 @@
 package com.lojaunit.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lojaunit.entities.Cliente;
 import com.lojaunit.services.ClienteService;
@@ -28,6 +34,25 @@ public class ClienteController {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Cliente> findById(@PathVariable("id") Long id){
 		Cliente cliente = clienteService.findById(id);
+		return ResponseEntity.ok().body(cliente);
+	}
+	
+	@PostMapping(value = "/add")
+	public ResponseEntity<Cliente> insert(@RequestBody Cliente cliente){
+		cliente = clienteService.insert(cliente);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cliente.getId()).toUri();
+		return ResponseEntity.created(uri).body(cliente);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		clienteService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Cliente> update(@PathVariable Long id, @RequestBody Cliente cliente){
+		cliente = clienteService.update(id, cliente);
 		return ResponseEntity.ok().body(cliente);
 	}
 }
--- src/main/java/com/lojaunit/services/ClienteService.java
+++ src/main/java/com/lojaunit/services/ClienteService.java
@@ -23,4 +23,29 @@ public class ClienteService {
 		Optional<Cliente> cliente = clienteRepository.findById(id);
 		return cliente.get();
 	}
+	
+	public Cliente insert(Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	public void delete(Long id) {
+		clienteRepository.deleteById(id);
+	}
+	
+	public Cliente update(Long id, Cliente cliente) {
+		Cliente cli = clienteRepository.getOne(id);
+		updateData(cli, cliente);
+		return clienteRepository.save(cli);
+	}
+
+	private void updateData(Cliente cli, Cliente cliente) {
+		cli.setCpf(cliente.getCpf());
+		cli.setNome(cliente.getNome());
+		cli.setEmail(cliente.getEmail());
+		cli.setDataNascimento(cliente.getDataNascimento());
+		cli.setSexo(cliente.getSexo());
+		cli.setNomeSocial(cliente.getNomeSocial());
+		cli.setApelido(cliente.getApelido());
+		cli.setTelefone(cliente.getTelefone());
+	}
 }
