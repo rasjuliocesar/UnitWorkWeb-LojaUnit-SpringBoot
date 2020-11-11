61a5a9a1afab9038906448c362a154cb668fa3ee
Julio Cesar <rasjuliocesar@gmail.com>
2020-11-10 21:09:43 -0300

    Post/Delete e Put - Fornecedores

--- src/main/java/com/lojaunit/controller/FornecedorController.java
+++ src/main/java/com/lojaunit/controller/FornecedorController.java
@@ -1,13 +1,19 @@
 package com.lojaunit.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lojaunit.entities.Fornecedor;
 import com.lojaunit.services.FornecedorService;
@@ -28,6 +34,25 @@ public class FornecedorController {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Fornecedor> findById(@PathVariable("id") Long id){
 		Fornecedor fornecedor = fornecedorService.findById(id);
+		return ResponseEntity.ok().body(fornecedor);
+	}
+	
+	@PostMapping(value = "/add")
+	public ResponseEntity<Fornecedor> insert(@RequestBody Fornecedor fornecedor){
+		fornecedor = fornecedorService.insert(fornecedor);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(fornecedor.getId()).toUri();
+		return ResponseEntity.created(uri).body(fornecedor);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		fornecedorService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Fornecedor> update(@PathVariable Long id, @RequestBody Fornecedor fornecedor){
+		fornecedor = fornecedorService.update(id, fornecedor);
 		return ResponseEntity.ok().body(fornecedor);
 	}
 }
--- src/main/java/com/lojaunit/services/FornecedorService.java
+++ src/main/java/com/lojaunit/services/FornecedorService.java
@@ -22,7 +22,27 @@ public class FornecedorService {
 	public Fornecedor findById(Long id) {
 		Optional<Fornecedor> fornecedor = fornecedorRepository.findById(id);
 		return fornecedor.get();
-		
-		
+	}
+	
+	public Fornecedor insert(Fornecedor fornecedor) {
+		return fornecedorRepository.save(fornecedor);
+	}
+	
+	public void delete(Long id) {
+		fornecedorRepository.deleteById(id);
+	}
+	
+	public Fornecedor update(Long id, Fornecedor fornecedor) {
+		Fornecedor forn = fornecedorRepository.getOne(id);
+		updateData(forn, fornecedor);
+		return fornecedorRepository.save(forn);
+	}
+	
+	private void updateData(Fornecedor forn, Fornecedor fornecedor) {
+		forn.setNome(fornecedor.getNome());
+		forn.setEndereco(fornecedor.getEndereco());
+		forn.setTelefone(fornecedor.getTelefone());
+		forn.setCnpj(fornecedor.getCnpj());
+		forn.setEmail(fornecedor.getEmail());
 	}
 }
