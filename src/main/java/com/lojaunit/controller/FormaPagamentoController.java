1023a4aabdb37e50825beea6f762c715197f44b0
Julio Cesar <rasjuliocesar@gmail.com>
2020-11-17 20:49:36 -0300

    Post/Delete e Put - FormaPagamento

--- src/main/java/com/lojaunit/controller/FormaPagamentoController.java
+++ src/main/java/com/lojaunit/controller/FormaPagamentoController.java
@@ -1,13 +1,19 @@
 package com.lojaunit.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lojaunit.entities.FormaPagamento;
 import com.lojaunit.services.FormaPagamentoService;
@@ -28,6 +34,25 @@ public class FormaPagamentoController {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<FormaPagamento> findById(@PathVariable("id") Long id){
 		FormaPagamento formaPagamento = formaPagamentoService.findById(id);
+		return ResponseEntity.ok().body(formaPagamento);
+	}
+	
+	@PostMapping(value = "/add")
+	public ResponseEntity<FormaPagamento> insert(@RequestBody FormaPagamento formaPagamento){
+		formaPagamento = formaPagamentoService.insert(formaPagamento);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(formaPagamento.getId()).toUri();
+		return ResponseEntity.created(uri).body(formaPagamento);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		formaPagamentoService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<FormaPagamento> update(@PathVariable Long id, @RequestBody FormaPagamento formaPagamento){
+		formaPagamento = formaPagamentoService.update(id, formaPagamento);
 		return ResponseEntity.ok().body(formaPagamento);
 	}
 }
--- src/main/java/com/lojaunit/services/FormaPagamentoService.java
+++ src/main/java/com/lojaunit/services/FormaPagamentoService.java
@@ -23,4 +23,24 @@ public class FormaPagamentoService {
 		Optional<FormaPagamento> formaPagamento = formaPagamentoRepository.findById(id);
 		return formaPagamento.get();
 	}
+	
+	public FormaPagamento insert(FormaPagamento formaPagamento) {
+		return formaPagamentoRepository.save(formaPagamento);
+	}
+	
+	public void delete(Long id) {
+		formaPagamentoRepository.deleteById(id);
+	}
+	
+	public FormaPagamento update(Long id, FormaPagamento formaPagamento) {
+		FormaPagamento formaPag = formaPagamentoRepository.getOne(id);
+		updateDate(formaPag, formaPagamento);
+		return formaPagamentoRepository.save(formaPag);
+	}
+
+	private void updateDate(FormaPagamento formaPag, FormaPagamento formaPagamento) {
+		formaPag.setForma(formaPagamento.getForma());
+		formaPag.setDescricao(formaPagamento.getDescricao());
+		formaPag.setAtivo(formaPagamento.getAtivo());
+	}
 }
