52c51f831ca35dad6489a6e01b7487512a5e1c5a
Julio Cesar <rasjuliocesar@gmail.com>
2020-11-17 20:37:54 -0300

    Post/Delete e Put - Marca

--- src/main/java/com/lojaunit/controller/MarcaController.java
+++ src/main/java/com/lojaunit/controller/MarcaController.java
@@ -1,13 +1,19 @@
 package com.lojaunit.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lojaunit.entities.Marca;
 import com.lojaunit.services.MarcaService;
@@ -28,6 +34,25 @@ public class MarcaController {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Marca> findById(@PathVariable("id") Long id){
 		Marca marca = marcaService.findById(id);
+		return ResponseEntity.ok().body(marca);
+	}
+	
+	@PostMapping(value = "/add")
+	public ResponseEntity<Marca> insert(@RequestBody Marca marca){
+		marca = marcaService.insert(marca);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(marca.getId()).toUri();
+		return ResponseEntity.created(uri).body(marca);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		marcaService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Marca> update(@PathVariable Long id, @RequestBody Marca marca){
+		marca = marcaService.update(id, marca);
 		return ResponseEntity.ok().body(marca);
 	}
 }
--- src/main/java/com/lojaunit/services/MarcaService.java
+++ src/main/java/com/lojaunit/services/MarcaService.java
@@ -23,4 +23,23 @@ public class MarcaService {
 		Optional<Marca> marca = marcaRepository.findById(id);
 		return marca.get();
 	}
+	
+	public Marca insert(Marca marca) {
+		return marcaRepository.save(marca);
+	}
+	
+	public void delete(Long id) {
+		marcaRepository.deleteById(id);
+	}
+	
+	public Marca update(Long id, Marca marca) {
+		Marca mar = marcaRepository.getOne(id);
+		updateData(mar, marca);
+		return marcaRepository.save(mar);
+	}
+
+	private void updateData(Marca mar, Marca marca) {
+		mar.setNome(marca.getNome());
+		mar.setDescricao(marca.getDescricao());
+	}
 }
